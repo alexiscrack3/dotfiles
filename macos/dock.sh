8b86f21dbc593b70acb6b447ff12c1947b41a6f3
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-02 09:24:03 -0600

    Update application's paths in dock script

--- macos/dock.sh
+++ macos/dock.sh
@@ -6,11 +6,11 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Android Studio.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/iTunes.app"
+dockutil --no-restart --add "/System/Applications/Music.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/App Store.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/App Store.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "~" --section others
 
 killall Dock
