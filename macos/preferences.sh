a2f687f2789f2e3977b4c78eef03e4a63fda09ff
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-14 09:45:29 -0700

    Add trackpad settings

--- macos/preferences.sh
+++ macos/preferences.sh
@@ -11,3 +11,10 @@ defaults write NSGlobalDomain AppleLanguages -array "en-US" "en"
 
 defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>15000</integer><key>KeyboardLayout Name</key><string>USInternational-PC</string></dict>'
 defaults write com.apple.HIToolbox AppleSelectedInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>15000</integer><key>KeyboardLayout Name</key><string>USInternational-PC</string></dict>'
+
+###############################################################################
+# Trackpad                                                                    #
+###############################################################################
+
+defaults write com.apple.universalaccess closeViewZoomDisplayID -bool true
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
