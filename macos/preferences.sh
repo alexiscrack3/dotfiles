807af4b15c27590acb4b55a4b6d4e0fbaed56154
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-14 09:46:30 -0700

    Add dock settings

--- macos/preferences.sh
+++ macos/preferences.sh
@@ -25,3 +25,9 @@ defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Battery.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+defaults write com.apple.dock show-recents -bool FALSE
