82d61a2de755ecf1e696508796d6521e1b76a5a0
Alexis Ortega <alexis.oc3@gmail.com>
2020-09-14 09:45:04 -0700

    Add keyboard settings

--- macos/preferences.sh
+++ macos/preferences.sh
@@ -4,3 +4,10 @@
 ###############################################################################
 
 defaults write NSGlobalDomain AppleLanguages -array "en-US" "en"
+
+###############################################################################
+# Keyboard                                                                    #
+###############################################################################
+
+defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>15000</integer><key>KeyboardLayout Name</key><string>USInternational-PC</string></dict>'
+defaults write com.apple.HIToolbox AppleSelectedInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>15000</integer><key>KeyboardLayout Name</key><string>USInternational-PC</string></dict>'
