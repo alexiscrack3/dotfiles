98952ebb4714c524ec7d4b36c35c200f7ff61c80
Alexis Ortega <alexis.oc3@gmail.com>
2021-04-14 12:38:15 -0600

    Change android to Kubernetes

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -6,7 +6,7 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 source "$DOTFILES_DIR/lib/colors.sh"
 
-echo "==> ${BOLD}Setting up android...${NORMAL}"
+echo "==> ${BOLD}Setting up Kubernetes...${NORMAL}"
 
 ln -sfv "$DOTFILES_DIR/kubernetes/.k8src" ~
 
