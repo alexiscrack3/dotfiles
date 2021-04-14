b8340ab634c250963bebb279402e71dfba4e31d2
Alexis Ortega <alexis.oc3@gmail.com>
2021-04-14 12:38:03 -0600

    Capilize Android

--- android/install.sh
+++ android/install.sh
@@ -6,7 +6,7 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 source "$DOTFILES_DIR/lib/colors.sh"
 
-echo "==> ${BOLD}Setting up android...${NORMAL}"
+echo "==> ${BOLD}Setting up Android...${NORMAL}"
 
 ln -sfv "$DOTFILES_DIR/android/.androidrc" ~
 
