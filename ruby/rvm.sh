12592bd851eeb417cd8fee742aab902f5249673d
Alexis Ortega <alexis.oc3@gmail.com>
2022-06-30 15:38:22 -0600

    Update ruby installation

--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -5,10 +5,12 @@ echo "==> ${BOLD}Installing rvm...${NORMAL}"
 if test ! $(which rvm); then
     echo "Installing rvm"
     \curl -L https://get.rvm.io | bash -s stable --auto-dotfiles
-    
-    source ~/.rvm/scripts/rvm
-
-    rvm use ruby --install --default
 else
     echo "rvm is already installed"
 fi
+
+# This loads rvm
+source ~/.rvm/scripts/rvm
+
+# Install the default version of ruby
+rvm use ruby --install --default
