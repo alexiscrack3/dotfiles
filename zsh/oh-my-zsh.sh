9d08505a6543643e314c1f1ea838994b974d077e
Alexis Ortega <alexis.oc3@gmail.com>
2022-07-06 09:59:20 -0600

    Update oh-my-zsh script

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,7 +1,6 @@
-ls ~/.oh-my-zsh &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Installing oh my zsh"
-    0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "==> ${BOLD}Installing oh my zsh...${NORMAL}"
+    0>/dev/null sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo "oh my zsh is already installed"
 fi
