c814d74038baed4bb765b1086c092bd302183ffc
Alexis Ortega <alexis.oc3@gmail.com>
2026-08-05 23:42:33 -0600

    Wrap the bare clone in an existence guard for Powerlevel10k

--- zsh/powerlevel10k.sh
+++ zsh/powerlevel10k.sh
@@ -1,1 +1,10 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+P10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+if [ ! -d "$P10K_DIR" ]; then
+    echo "==> Installing powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+else
+    echo "powerlevel10k is already installed"
+fi
+
+unset P10K_DIR
