15fcb18a189f423612cb5ff75254cfea13a3ac20
Alexis Ortega <alexis.oc3@gmail.com>
2026-08-11 00:41:11 -0600

    Stop making nvm dir

--- node/nvm.sh
+++ node/nvm.sh
@@ -5,7 +5,6 @@ NVM_DIR="$HOME/.nvm"
 if [ ! -d "$NVM_DIR" ]; then
     echo "==> Installing nvm"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
-    mkdir $NVM_DIR
 else
     echo "Directory $NVM_DIR already exists"
 fi
